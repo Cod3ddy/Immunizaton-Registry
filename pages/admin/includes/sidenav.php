1b3734712966e0c9033ac3072606b6a809f8aeef
Shadow <edwardchirwa07@gmail.com>
2023-11-17 00:38:12 +0200

    restructured the project

---
+++ pages/admin/includes/sidenav.php
@@ -0,0 +1,20 @@
+<!-- side nav [admin dashboard] -->
+<div class="side-navigation">
+    <!-- side navigation bar content -->
+    <div class="content">
+        <ul class="nav-links">
+            <li class="nav-dashboard"><i class="bi bi-grid"></i>Dashboard</li>
+            <li class="nav-admin-btn"><i class="bi bi-map"></i>admin</li>
+            <li class="nav-admin-btn">
+                <i class="bi bi-calendar-check"></i>admin
+            </li>
+            <li class="nav-profile-btn"><i class="bi bi-person"></i>Profile</li>
+        </ul>
+
+        <div class="nav-footer">
+            <div class="nav-footer-item">
+                <div class="fa-solid fa-info"></div>
+            </div>
+        </div>
+    </div>
+</div>
