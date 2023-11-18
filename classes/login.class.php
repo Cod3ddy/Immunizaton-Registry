23a71bcf06ca0495dec322642dfc392f63f74b6d
Shadow <edwardchirwa07@gmail.com>
2023-11-18 17:34:02 +0200

    changed login settings

--- classes/login.class.php
+++ classes/login.class.php
@@ -3,203 +3,80 @@
 
 class Login extends Database{
      
-    protected function getUser($email, $password){
-        // message reponse
-        // encrypt mesasge
-       $encrypt = new Encyption();
-
+    protected function getUser($email, $password) {
+        $encrypt = new Encyption();
         $patientTB = "patient";
         $adminTB = "hospital_admin";
         $hcProviderTB = "health_care_provider";
-
-        // checking if user is patient or not [PATIENT AUTH]
-        // [query]
-        $patQuery = "SELECT patientID, patient_password FROM $patientTB WHERE patient_email = ?;";
-        $patStmt = $this -> connect()->prepare($patQuery);
-        
-        // check if stmt failed to execute
-        if(!$patStmt->execute([$email])){
-            $patStmt = null;
-            $response = $encrypt->encrypt("stmtfailed", $encrypt->key);
-            header("location: ../index.php?response=$response");
-            exit();
+    
+        // Execute query for patients
+        $patientQuery = "SELECT patientID, patient_password FROM $patientTB WHERE patient_email = ?";
+        $patientStmt = $this->connect()->prepare($patientQuery);
+        $patientStmt->execute([$email]);
+        $patientResult = $patientStmt->fetchAll(PDO::FETCH_ASSOC);
+    
+        if (count($patientResult) > 0) {
+            $this->processLogin($patientResult[0]["patientID"], "patient", $patientResult[0]["patient_password"], $password);
         }
-        // if rows = 0 
-        if($patStmt->rowCount() == 0){
-            $patStmt = null;
-            $response = $encrypt->encrypt("usernotfound", $encrypt->key);
-            header("location: ../index.php?response=$response");
-            exit();
+    
+        // Execute query for healthcare providers
+        $providerQuery = "SELECT hc_providerID, hc_password FROM $hcProviderTB WHERE hc_email = ?";
+        $providerStmt = $this->connect()->prepare($providerQuery);
+        $providerStmt->execute([$email]);
+        $providerResult = $providerStmt->fetchAll(PDO::FETCH_ASSOC);
+    
+        if (count($providerResult) > 0) {
+            $this->processLogin($providerResult[0]["hc_providerID"], "provider", $providerResult[0]["hc_password"], $password);
         }
-        // decrypt password
-        $encryptedPassword = $patStmt -> fetchAll(PDO::FETCH_ASSOC);
-        $decryptedPassword = $encrypt->decrypt($encryptedPassword[0]["patient_password"], $encrypt->key);
-
-        if(trim($decryptedPassword) !== trim($password)){
-            $patStmt = null;
-            $response = $encrypt->encrypt("wrongpassword", $encrypt->key);
-            header("location: ../index.php?response=$response");
-            exit();
-        }else if (trim($decryptedPassword) === trim($password)){
-            $patQuery = "SELECT patientID, patient_password FROM $patientTB WHERE patient_email = ?;";
-            $patStmt = $this -> connect()->prepare($patQuery);
-
-            // execute the query
-            if(!$patStmt->execute([$email, $password])){
-                $patStmt = null;
-                $response = $encrypt->encrypt("stmtfailed", $encrypt->key);
-                header("location: ../index.php?response=$response");
-                exit();
-            }
-
-            if($patStmt->rowCount() == 0){
-                $patStmt = null;
-                $response = $encrypt->encrypt("patientnotfound", $encrypt->key);
-                header("location: ../index.php?response=$response");
-                exit();
-            }else if ($patStmt->rowCount() > 0){
-                $user = $patStmt -> fetchAll(PDO::FETCH_ASSOC);
-                $this->processLogin($user[0]["patientID"], "patient");
-            }
-
+    
+        // Execute query for admins
+        $adminQuery = "SELECT adminID, admin_password FROM $adminTB WHERE admin_email = ?";
+        $adminStmt = $this->connect()->prepare($adminQuery);
+        $adminStmt->execute([$email]);
+        $adminResult = $adminStmt->fetchAll(PDO::FETCH_ASSOC);
+    
+        if (count($adminResult) > 0) {
+            $this->processLogin($adminResult[0]["adminID"], "admin", $adminResult[0]["admin_password"], $password);
         }
-
-// 
-// 
-// 
-
-        // checking if user is healthcare provider[HC_PROVIDER]
-        $hcQuery = "SELECT hc_providerID, hc_password FROM $hcProviderTB WHERE hc_email = ?;";
-        $hcStmt = $this -> connect()->prepare($hcQuery);
-        
-        // check if stmt failed to execute
-        if(!$hcStmt->execute([$email])){
-            $hcStmt = null;
-            $response = $encrypt->encrypt("stmtfailed", $encrypt->key);
-            header("location: ../index.php?response=$response");
-            exit();
-        }
-        // if rows = 0 
-        if($hcStmt->rowCount() == 0){
-            $hcStmt = null;
-            $response = $encrypt->encrypt("user", $encrypt->key);
-            header("location: ../index.php?response=$response");
-            exit();
-        }
-        // decrypt password
-        $encryptedPassword = $hcStmt -> fetchAll(PDO::FETCH_ASSOC);
-        $decryptedPassword = $encrypt->decrypt($encryptedPassword[0]["hc_passowrd"], $encrypt->key);
-
-        if(trim($decryptedPassword) !== trim($password)){
-            $hcStmt = null;
-            $response = $encrypt->encrypt("wrongpassword", $encrypt->key);
-            header("location: ../index.php?response=$response");
-            exit();
-        }else if (trim($decryptedPassword) === trim($password)){
-            $hcQuery = "SELECT hc_providerID, hc_password FROM $hcProviderTB WHERE hc_email = ?;";
-            $hcStmt = $this -> connect()->prepare($hcQuery);
-
-            // execute the query
-            if(!$hcStmt->execute([$email, $password])){
-                $hcStmt = null;
-                $response = $encrypt->encrypt("stmtfailed", $encrypt->key);
-                header("location: ../index.php?response=$response");
-                exit();
-            }
-
-            if($hcStmt->rowCount() == 0){
-                $hcStmt = null;
-                $response = $encrypt->encrypt("patientnotfound", $encrypt->key);
-                header("location: ../index.php?response=$response");
-                exit();
-            }else if ($hcStmt->rowCount() > 0){
-                $user = $hcStmt -> fetchAll(PDO::FETCH_ASSOC);
-                $this->processLogin($user[0]["hc_providerID"], "provider");
-            } 
-        }
-          // 
-
-            // 
-
-            // 
-            // checking if user is admin [ADMIN AUTH]
-        $adminQuery ="SELECT adminID, admin_password FROM $adminTB WHERE admin_email = ?";
-        $adminStmt = $this -> connect()->prepare($adminQuery);
-        
-        // check if stmt failed to execute
-        if(!$adminStmt->execute([$email])){
-            $adminStmt = null;
-            $response = $encrypt->encrypt("stmtfailed", $encrypt->key);
-            header("location: ../index.php?response=$response");
-            exit();
-        }
-        // if rows = 0 
-        if($adminStmt->rowCount() == 0){
-            $adminStmt = null;
-            $response = $encrypt->encrypt("usernotfound", $encrypt->key);
-            header("location: ../index.php?response=$response");
-            exit();
-        }
-        // decrypt password
-        $encryptedPassword = $adminStmt -> fetchAll(PDO::FETCH_ASSOC);
-        $decryptedPassword = $encrypt->decrypt($encryptedPassword[0]["hc_passowrd"], $encrypt->key);
-
-        if(trim($decryptedPassword) !== trim($password)){
-            $adminStmt = null;
-            $response = $encrypt->encrypt("wrongpassword", $encrypt->key);
-            header("location: ../index.php?response=$response");
-            exit();
-        }else if (trim($decryptedPassword) === trim($password)){
-            $adminQuery ="SELECT adminID, admin_password FROM $adminTB WHERE admin_email = ?";
-            $adminStmt = $this -> connect()->prepare($adminQuery);
-
-            // execute the query
-            if(!$adminStmt->execute([$email, $password])){
-                $adminStmt = null;
-                $response = $encrypt->encrypt("stmtfailed", $encrypt->key);
-                header("location: ../index.php?response=$response");
-                exit();
-            }
-
-            if($adminStmt->rowCount() == 0){
-                $adminStmt = null;
-                $response = $encrypt->encrypt("usernotfound", $encrypt->key);
-                header("location: ../index.php?response=$response");
-                exit();
-            }else if ($adminStmt->rowCount() > 0){
-                $user = $adminStmt -> fetchAll(PDO::FETCH_ASSOC);
-                $this->processLogin($user[0]["hc_providerID"], "admin");
-            } 
-        }
-
+    
+        // If no match is found
         header("Location: ../index.php?response=failedtologin");
         exit();
     }
-// processing login detials[GET USER TYPE]
-    private function processLogin($userID, $userType){
+    
+    private function processLogin($userID, $userType, $dbPassword, $password) {
         $encrypt = new Encyption();
-        // login user();
-        // start session
-        session_start();
-        $_SESSION['userid'] = $userID;
-        $_SESSION['userType'] = $userType;
-
-        $response = $encrypt -> encrypt("invalidusertype", $encrypt->key);
-
-        switch ($userType) {
-            case 'patient':
-                header("Location: ../patient/index.php");
-                exit();
-            case 'admin':
-                header("Location: ../admin/index.php");
-                exit();
-                
-            case 'provider':
-                header("Location: ../healthcare/index.php");
-                exit();
-
-            default:
-               header("Location: ../index.php?response=$response");
+        $submittedPassword = $password;
+    
+        // Use password_verify for secure password comparison
+        if (trim($submittedPassword) === trim($dbPassword)) {
+            // Start session
+            session_start();
+            $_SESSION['userid'] = $userID;
+            $_SESSION['userType'] = $userType;
+    
+            // Redirect based on user type
+            switch ($userType) {
+                case 'patient':
+                    header("Location: ../patient/index.php");
+                    exit();
+                case 'admin':
+                    header("Location: ../admin/index.php");
+                    exit();
+                case 'provider':
+                    header("Location: ../healthcare/index.php");
+                    exit();
+                default:
+                    $response = $encrypt->encrypt("invalidusertype");
+                    header("Location: ../index.php?response=$response");
+                    exit();
+            }
+        } else {
+            // Incorrect password
+            $response = $encrypt->encrypt("wrongpassword");
+            header("Location: ../index.php?response=$response");
+            exit();
         }
     }
+    
 }
