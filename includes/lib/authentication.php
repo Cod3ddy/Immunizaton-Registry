1bbef6a33735a71997e66355dc70816d6429ce3a
Shadow <edwardchirwa07@gmail.com>
2023-11-18 17:32:12 +0200

    refactored encryption class

--- includes/lib/authentication.php
+++ includes/lib/authentication.php
@@ -4,16 +4,16 @@ class Encyption{
     // encryption and decyption key
     public $key = '@cod3ddyspookymanwodahsqwertyhandszxcwoopy24gang';
     // encrypt data
-    public function encrypt($data, $key){
-        $encription_key = base64_decode($key);
+    public function encrypt($data){
+        $encription_key = base64_decode($this -> key);
         $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length("aes-256-cbc"));
         $encripted = openssl_encrypt($data, 'aes-256-cbc', $encription_key, 0, $iv);
         return base64_encode($encripted.'::'.$iv);
     }
 
     // decrypt data
-    public function decrypt($data, $key){
-        $encryption_key = base64_decode($key);
+    public function decrypt($data){
+        $encryption_key = base64_decode($this -> key);
         list($encripted_data, $iv) = array_pad(explode('::', base64_decode($data), 2),2, null);
         return openssl_decrypt($encripted_data, 'aes-256-cbc', $encryption_key, 0, $iv);
     }
